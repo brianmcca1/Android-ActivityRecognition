bba8a83f85447e9c8927ad020b7c0c9dab3382ba
kicknickr <kicknickr2@gmail.com>
2018-02-09 17:35:17 -0500

    ActivityRecogSerivce broadcasts intents

--- app/src/main/java/com/tutsplus/activityrecognition/ActivityRecognizedService.java
+++ app/src/main/java/com/tutsplus/activityrecognition/ActivityRecognizedService.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -20,6 +21,10 @@ public class ActivityRecognizedService extends IntentService {
         super("ActivityRecognizedService");
     }
 
+    private final double ACTIVITY_RECOG_MIN_THRESH = 0.75;
+    private final List<Integer> ACCEPTED_ACTIVITIES = Arrays.asList(DetectedActivity.STILL, DetectedActivity.WALKING, DetectedActivity.RUNNING);
+    public final String ACTION_ACTIVITY_RECOG = "ACTION_ACTIVITY_RECOG";
+    public final String INTENT_EXTRA_ACTIVITY_RECOG = "INTENT_EXTRA_ACTIVITY_RECOG";
     public ActivityRecognizedService(String name) {
         super(name);
     }
@@ -28,53 +33,19 @@ public class ActivityRecognizedService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if(ActivityRecognitionResult.hasResult(intent)) {
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
-            handleDetectedActivities( result.getProbableActivities() );
+            handleDetectedActivities(result);
         }
     }
 
-    private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
-        for( DetectedActivity activity : probableActivities ) {
-            switch( activity.getType() ) {
-                case DetectedActivity.IN_VEHICLE: {
-                    Log.e( "ActivityRecogition", "In Vehicle: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.ON_BICYCLE: {
-                    Log.e( "ActivityRecogition", "On Bicycle: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.ON_FOOT: {
-                    Log.e( "ActivityRecogition", "On Foot: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.RUNNING: {
-                    Log.e( "ActivityRecogition", "Running: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.STILL: {
-                    Log.e( "ActivityRecogition", "Still: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.TILTING: {
-                    Log.e( "ActivityRecogition", "Tilting: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.WALKING: {
-                    Log.e( "ActivityRecogition", "Walking: " + activity.getConfidence() );
-                    if( activity.getConfidence() >= 75 ) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText( "Are you walking?" );
-                        builder.setSmallIcon( R.mipmap.ic_launcher );
-                        builder.setContentTitle( getString( R.string.app_name ) );
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                    }
-                    break;
-                }
-                case DetectedActivity.UNKNOWN: {
-                    Log.e( "ActivityRecogition", "Unknown: " + activity.getConfidence() );
-                    break;
-                }
+    private void handleDetectedActivities(ActivityRecognitionResult result) {
+
+        DetectedActivity mostProbablyActivity = result.getMostProbableActivity();
+            if(ACCEPTED_ACTIVITIES.contains(mostProbablyActivity.getType()) &&
+                    mostProbablyActivity.getConfidence() > ACTIVITY_RECOG_MIN_THRESH) {
+                Intent intent = new Intent();
+                intent.setAction(ACTION_ACTIVITY_RECOG);
+                intent.putExtra(INTENT_EXTRA_ACTIVITY_RECOG, mostProbablyActivity.getType());
+                sendBroadcast(intent);
             }
         }
-    }
 }
